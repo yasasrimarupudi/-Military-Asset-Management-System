d455a48080eee37caa7861c9c7689708af2b0fdc
yasasrimarupudi <114413766+yasasrimarupudi@users.noreply.github.com>
2025-05-26 17:24:26 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // adjust your backend URL
+  baseURL: process.env.REACT_APP_BACKEND_URL + '/api',
 });
 
 export const setAuthToken = (token) => {
